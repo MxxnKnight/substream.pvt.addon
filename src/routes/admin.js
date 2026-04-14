bac8f33c4f6ac0f9691e7a118f7a91211e32de89
MӨӨП KПIGΉƬ <91146761+MxxnKnight@users.noreply.github.com>
2026-04-14 18:32:04 +0530

    fix: add missing inspectExternalLink import in admin routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
 const { uploadSubtitle } = require('../controllers/uploadController');
-const { listSubtitles, deleteSubtitle, fetchMetadata, searchExternalSubtitles, importExternalSubtitle } = require('../controllers/adminController');
+const { listSubtitles, deleteSubtitle, fetchMetadata, searchExternalSubtitles, importExternalSubtitle, inspectExternalLink } = require('../controllers/adminController');
 const { authenticate } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
